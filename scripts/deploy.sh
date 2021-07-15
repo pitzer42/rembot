da1ce8a7375abc1c294127209fbf0744f8c12664
Arthur Pitzer <arthurpitzer@id.uff.br>
2021-07-15 19:16:41 +0000

    heroku deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
-heroku login -i
-
 heroku create rembot-dev
 heroku git:remote -a rembot-dev
 
@@ -9,7 +7,6 @@ git commit -m 'heroku deploy'
 git push heroku
 
 heroku logs --tail
-
 
 # Create Config Vars
 # ex.: heroku config:set GITHUB_USERNAME=joesmith
